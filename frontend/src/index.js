bceb89c97b9aa88ef9ef6766be95c9ad4d8f8b41
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-04-12 23:38:43 +0200

    Reset styles and created global styles with styled components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ResetStyles from './styles/reset.styled';
+import GlobalStyles from './styles/global.styled';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <ResetStyles />
+      <GlobalStyles />
+      <App />
+    </>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
---
+++ frontend/src/styles/global.styled.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  h1 {
+    font-size: 24px;
+  }
+`;
+
+export default GlobalStyles;
