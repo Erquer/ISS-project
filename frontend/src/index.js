7f15f31e6afafbcc3dbcd8d55848b84608a1e5f5
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-04-13 09:43:22 +0200

    Created theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.styled';
 import ResetStyles from './styles/reset.styled';
 import GlobalStyles from './styles/global.styled';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
-      <ResetStyles />
-      <GlobalStyles />
-      <App />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <ResetStyles />
+        <GlobalStyles />
+        <App />
+      </>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/styles/theme.styled.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#B2BAFF',
+    secondary: '#495AF2',
+    backgroundLight: '#222A70',
+    backgroundDark: '#000634',
+    textLight: '#FFFFFF',
+    text: '#DDDDDD',
+  },
+};
+
+export default theme;
